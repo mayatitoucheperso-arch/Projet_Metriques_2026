ddfe6c014d2e92fd80804c95d13aaec42aaa5fe2
mayatitoucheperso-arch <mayatitouche.perso@gmail.com>
2026-03-18 11:06:59 +0000

    MAJ

--- app.py
+++ app.py
@@ -9,11 +9,55 @@ def hello_world():
 
 # Déposez votre code à partir d'ici :
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
+@app.get("/paris")
+def api_paris():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8566&longitude=2.3522&hourly=temperature_2m"
+    response = requests.get(url)
+    data = response.json()
 
+    times = data.get("hourly", {}).get("time", [])
+    temps = data.get("hourly", {}).get("temperature_2m", [])
 
+    n = min(len(times), len(temps))
+    result = [
+        {"datetime": times[i], "temperature_c": temps[i]}
+        for i in range(n)
+    ]
 
+    return jsonify(result)
+@app.route("/rapport")
+def mongraphique():
+    return render_template("graphique.html")
 
+@app.route("/histogramme")
+def histogramme():
+    return render_template("histogramme.html")
+
+@app.get("/versailles")
+def api_versailles():
+    url = "https://api.open-meteo.com/v1/forecast?latitude=48.8049&longitude=2.1204&hourly=relative_humidity_2m"
+    response = requests.get(url)
+    data = response.json()
+
+    times = data.get("hourly", {}).get("time", [])
+    humidites = data.get("hourly", {}).get("relative_humidity_2m", [])
+
+    n = min(len(times), len(humidites))
+    result = [
+        {"datetime": times[i], "humidite": humidites[i]}
+        for i in range(n)
+    ]
+
+    return jsonify(result)
+
+@app.route("/atelier")
+def atelier():
+    return render_template("atelier.html")
+   
 # Ne rien mettre après ce commentaire
     
 if __name__ == "__main__":
